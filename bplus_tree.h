2c299be7a8f2c60d89d8dbd7e9e85b24088f2855
妖冶如你 <yaoyeruni@gmail.com>
2021-09-22 20:38:33 +0800

    Add the bplus-tree erase operation

--- bplus_tree.h
+++ bplus_tree.h
@@ -16,6 +16,13 @@ public:
     struct node;
     typedef std::pair<node*, int> pos_type;
 
+    struct info {
+        void clear() { first = last = nullptr; size = 0; }
+        node *first = nullptr;
+        node *last = nullptr;
+        size_t size = 0;
+    };
+
     struct node {
         node(int M, bool isleaf) : isleaf(isleaf)
         {
@@ -30,7 +37,7 @@ public:
             if (isleaf) return { this, i };
             return childs[i]->find(key);
         }
-        pos_type insert(const key_type& key, const value_type& value)
+        void insert(const key_type& key, const value_type& value, info& info)
         {
             int i = search(key);
             if (isleaf) {
@@ -43,9 +50,15 @@ public:
                     }
                     keys[i] = key;
                     values[i] = new value_type(value);
+                    if (!info.first || less(key, info.first->keys[0])) {
+                        info.first = this;
+                    }
+                    if (!info.last || less(info.last->keys[info.last->n - 1], key)) {
+                        info.last = this;
+                    }
+                    info.size++;
                     n++;
                 }
-                return { this, i };
             } else {
                 if (i == n) {
                     // 如果key比索引节点中最大的关键字还要大，那么就需要更新索引节点的右边界
@@ -54,9 +67,10 @@ public:
                 // 分裂沿途的满节点
                 if (childs[i]->isfull()) {
                     split(i);
+                    if (info.last == childs[i]) info.last = childs[i + 1];
                     if (less(keys[i], key)) i++;
                 }
-                return childs[i]->insert(key, value);
+                childs[i]->insert(key, value, info);
             }
         }
         void split(int i)
@@ -95,6 +109,107 @@ public:
             x->n = M - t;
             n = t;
             return x;
+        }
+        // 为了防止合并叶子节点后导致父节点中的关键字数目少于ceil(M / 2)
+        // 我们在沿树下降过程中提前合并以保证降临的节点的关键字数目至少为ceil(M / 2) + 1
+        // 这样就不需要再进行回溯了
+        void erase(const key_type& key, node *precursor, info& info)
+        {
+            int i = search(key);
+            if (i == n) return;
+            if (isleaf) {
+                if (isequal(i, key)) {
+                    remove_key(i, true);
+                    info.size--;
+                }
+                return;
+            }
+            node *x = childs[i];
+            if (!precursor && isequal(i, key)) {
+                precursor = x->get_precursor();
+            }
+            if (precursor) {
+                keys[i] = precursor->keys[precursor->n - 2];
+            }
+            int M = keys.size();
+            int t = ceil(M / 2.0);
+            if (x->n == t) {
+                node *y = i - 1 >= 0 ? childs[i - 1] : nullptr;
+                node *z = i + 1 < n ? childs[i + 1] : nullptr;
+                if (y && y->n > t) {
+                    borrow_from_left(x, y, i - 1);
+                    x->erase(key, precursor, info);
+                } else if (z && z->n > t) {
+                    borrow_from_right(x, z, i);
+                    x->erase(key, precursor, info);
+                } else {
+                    if (y) {
+                        node *c = childs[i - 1];
+                        remove_key(i - 1, false);
+                        childs[i - 1] = c;
+                        if (info.first == x) info.first = y;
+                        if (info.last == x) info.last = y;
+                        y->merge(x);
+                        y->erase(key, precursor, info);
+                    } else if (z) {
+                        node *c = childs[i];
+                        remove_key(i, false);
+                        childs[i] = c;
+                        if (info.first == z) info.first = x;
+                        if (info.last == z) info.last = x;
+                        x->merge(z);
+                        x->erase(key, precursor, info);
+                    }
+                }
+            } else {
+                x->erase(key, precursor, info);
+            }
+        }
+        node *get_precursor()
+        {
+            if (isleaf) return this;
+            return childs[n - 1]->get_precursor();
+        }
+        void borrow_from_right(node *x, node *z, int i)
+        {
+            copy(i, z, 0);
+            x->copy(x->n++, z, 0);
+            if (!x->isleaf) x->childs[x->n - 1] = z->childs[0];
+            z->remove_key(0, false);
+        }
+        void borrow_from_left(node *x, node *y, int i)
+        {
+            for (int j = x->n - 1; j >= 0; j--) {
+                x->copy(j + 1, j);
+                if (!x->isleaf) x->childs[j + 1] = x->childs[j];
+            }
+            x->copy(0, y, y->n - 1);
+            x->n++;
+            if (!x->isleaf) x->childs[0] = y->childs[y->n - 1];
+            y->remove_key(y->n - 1, false);
+            copy(i, y, y->n - 1);
+        }
+        void merge(node *x)
+        {
+            for (int j = 0; j < x->n; j++) {
+                copy(j + n, x, j);
+                if (!isleaf) childs[j + n] = x->childs[j];
+            }
+            if (isleaf) {
+                right = x->right;
+                if (x->right) x->right->left = this;
+            }
+            n += x->n;
+            delete x;
+        }
+        void remove_key(int i, bool deleted)
+        {
+            if (deleted) delete values[i];
+            for (int j = i + 1; j < n; j++) {
+                copy(j - 1, j);
+                if (!isleaf) childs[j - 1] = childs[j];
+            }
+            n--;
         }
 
         // 查找this->keys[]中大于等于key的关键字的索引位置
@@ -142,6 +257,7 @@ public:
 
     class iterator {
     public:
+        iterator() : pos(nullptr, 0) {  }
         iterator(node *x, int i) : pos(x, i) {  }
         bool valid() { return pos.first; }
         key_type *key() { return &pos.first->keys[pos.second]; }
@@ -180,15 +296,17 @@ public:
     bplus_tree(int M) : M(M), root(new node(M, true))
     {
     }
-    iterator first() { return iterator(begin, 0); }
-    iterator last() { return iterator(end, end ? end->n - 1 : 0); }
+    ~bplus_tree() { clear(); delete root; }
+    iterator first() { return iterator(info.first, 0); }
+    iterator last() { return iterator(info.last, info.last ? info.last->n - 1 : 0); }
+    size_t size() { return info.size; }
     iterator find(const key_type& key)
     {
         auto [x, i] = root->find(key);
         if (x && x->isequal(i, key)) return iterator(x, i);
-        return iterator(nullptr, 0);
+        return iterator();
     }
-    iterator insert(const key_type& key, const value_type& value)
+    void insert(const key_type& key, const value_type& value)
     {
         node *r = root;
         if (r->isfull()) {
@@ -196,14 +314,24 @@ public:
             root->childs[0] = r;
             root->split(0);
         }
-        auto [ins, i] = root->insert(key, value);
-        if (!begin || less(ins->keys[i], begin->keys[0])) {
-            begin = ins;
+        root->insert(key, value, info);
+    }
+    void erase(const key_type& key)
+    {
+        root->erase(key, nullptr, info);
+        if (!root->isleaf && root->n == 1) {
+            node *r = root->childs[0];
+            delete root;
+            root = r;
         }
-        if (!end || less(end->keys[end->n - 1], ins->keys[i])) {
-            end = ins;
+    }
+    void clear()
+    {
+        while (info.size > 0) {
+            auto key = *first().key();
+            erase(key);
         }
-        return iterator(ins, i);
+        info.clear();
     }
 private:
     bplus_tree(const bplus_tree&) = delete;
@@ -218,7 +346,7 @@ private:
     // 并且所有关键字必须都出现在叶子节点中
     const int M = 4; // M >= 3
     node *root = nullptr;
-    node *begin = nullptr, *end = nullptr;
+    info info;
     Comparator comparator;
 };
 
